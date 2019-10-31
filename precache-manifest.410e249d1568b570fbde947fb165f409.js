11101fd074f152f9105ca1f7bb5d6ee00efe79a2
Jimmy Chen <enjoy8062@gmail.com>
2019-11-01 00:33:57 +1030

    Updates

---
+++ precache-manifest.410e249d1568b570fbde947fb165f409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5646f108d0e9600d2bb17aba9cc0ce98",
+    "url": "/React_TodoList/index.html"
+  },
+  {
+    "revision": "dd7ea14276b5f27ebc8e",
+    "url": "/React_TodoList/static/css/main.ac0fc721.chunk.css"
+  },
+  {
+    "revision": "d72eaf8afc4ea5f82eaf",
+    "url": "/React_TodoList/static/js/2.c997a1e7.chunk.js"
+  },
+  {
+    "revision": "dd7ea14276b5f27ebc8e",
+    "url": "/React_TodoList/static/js/main.fe55417e.chunk.js"
+  },
+  {
+    "revision": "e3d58fdaf8dff6f11319",
+    "url": "/React_TodoList/static/js/runtime-main.81cd550a.js"
+  }
+]);
